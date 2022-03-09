4fc876022aae7fb7264c2b0f1a78618a0220c801
Aleksei Zolotykh <aleksei.zolotykh@syngenta.com>
2022-03-09 01:21:29 +0100

    Aff forward and backward navigation

--- lua/neoc/keymaps/init.lua
+++ lua/neoc/keymaps/init.lua
@@ -2,3 +2,5 @@ local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
 map('n', '<F5>', ':exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', default_opts)
+map('n', '<leader>[', '<C-O>', default_opts) -- backward navigation
+map('n', '<leader>[', '<C-I>', default_opts) -- forward naviagtion navigation
