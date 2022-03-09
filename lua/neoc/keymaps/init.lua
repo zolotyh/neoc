9d3c83847d56c5051ca1ec68bc66ff0f02857575
Aleksei Zolotykh <aleksei.zolotykh@syngenta.com>
2022-03-09 01:22:52 +0100

    Add backward and forward navigation

--- lua/neoc/keymaps/init.lua
+++ lua/neoc/keymaps/init.lua
@@ -3,4 +3,4 @@ local default_opts = {noremap = true, silent = true}
 
 map('n', '<F5>', ':exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', default_opts)
 map('n', '<leader>[', '<C-O>', default_opts) -- backward navigation
-map('n', '<leader>[', '<C-I>', default_opts) -- forward naviagtion navigation
+map('n', '<leader>]', '<C-I>', default_opts) -- forward naviagtion navigation
