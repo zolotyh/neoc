4e26b871722e5648f30cc59d65120159a321b6a3
Aleksei Zolotykh <aleksei.zolotykh@syngenta.com>
2022-03-12 17:50:21 +0100

    Fix config reloading

--- lua/neoc/core/reload.lua
+++ lua/neoc/core/reload.lua
@@ -2,14 +2,9 @@
 local module_name = '^neoc';
 
 return function()
-  for name, _ in pairs(package.loaded) do
-    if name:match(module_name) then
-      print(name);
-      package.loaded[name] = nil
-    end
-  end
+  for name, _ in pairs(package.loaded) do if name:match(module_name) then package.loaded[name] = nil end end
 
   dofile(vim.env.MYVIMRC)
-  vim.api.nvim_command(':PackerSync')
+  vim.api.nvim_command(':PackerCompile')
 end
 
